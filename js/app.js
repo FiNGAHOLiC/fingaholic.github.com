264c47c2847fe2e92f92733d0f6b1111597f046b
FiNGAHOLiC <pl4y4h4ter@gmail.com>
2012-04-26 22:20:53 +0900

    Site updated at 2012-04-26

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@
 				var js, fjs = d.getElementsByTagName(s)[0];
 				if (d.getElementById(id)) return;
 				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=248454418530104";
+				js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=414280305262678";
 				fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		}(jQuery));
