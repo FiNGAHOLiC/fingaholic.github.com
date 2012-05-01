10a3d3881eb74d928a7eb39cc4770a7e23cfcfe3
FiNGAHOLiC <pl4y4h4ter@gmail.com>
2012-05-01 16:55:59 +0900

    Add grunt fikle

---
+++ grunt.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		concat : {
+			'js/all.js' : [
+				'js/jquery-ui-1.8.18.custom.min.js',
+				'js/jquery.easing.1.3.js',
+				'js/jquery.ui.plugin.js',
+				'js/app.js'
+			]
+		}
+	});
+};
